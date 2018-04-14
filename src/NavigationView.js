bc7e40fd873ddd2eaa905d768f254ebd0ab1e1e7
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-04-14 11:12:49 +0300

    Valuttele data to nav view

--- src/NavigationView.js
+++ src/NavigationView.js
@@ -3,38 +3,71 @@ import * as classnames from 'classnames';
 import * as Utils from './util';
 import './NavigationView.css';
 
+const Loading = () => <p className="loading">plz wait while calculating...</p>;
+
+const ManeuverImage = ({ maneuver }) => (
+  <img
+    src={'img/' + maneuver + '.jpg'}
+    className="navigation__direction-image"
+    alt={maneuver}
+  />
+);
+
 const Distance = ({ meters }) => {
   const { distance, unit } = Utils.getDistanceInRandomUnit(meters);
   return <p className="distance">{`${distance.toFixed(1)} ${unit}`}</p>;
 };
 
-const Loading = () => <p className="loading">plz wait while calculating...</p>;
-
-const Instructions = ({ waypoint }) => {};
-
-const DirectionImage = imageString => (
-  <img
-    src={'img/' + imageString + '.jpg'}
-    className="direction__image"
-    alt={imageString}
+const TextInstructions = ({ htmlInstructions }) => (
+  <p
+    className="text-instructions"
+    dangerouslySetInnerHTML={{ __html: htmlInstructions }}
   />
 );
 
-const DirectionText = (textString) => (
-  <p className="navigation__direction-text">{textString}</p>
-);
+/* ----- example "directionStep" ---------
+"distance": {
+        "text": "0.3 km",
+        "value": 288
+    },
+    "duration": {
+        "text": "4 mins",
+        "value": 245
+    },
+    "end_location": {
+        "lat": 60.1697649,
+        "lng": 24.9267587
+    },
+    "html_instructions": "Head <b>east</b> on <b>Alkärrsgatan</b>/<b>Leppäsuonkatu</b> toward <b>Runeberginkatu</b>/<b>Runebergsgatan</b>",
+    "polyline": {
+        "points": "{xfnJspbwCFkCB{@Fg@Jc@mByQUeB"
+    },
+    "start_location": {
+        "lat": 60.1692601,
+        "lng": 24.9218569
+    },
+    "travel_mode": "WALKING"
+},
+*/
+const Navigation = ({ directionStep }) => {
+  const {
+    distance,
+    maneuver,
+    html_instructions: htmlInstructions,
+    start_location: startCoords,
+    end_location: endCoords
+  } = directionStep;
 
-const DirectionImage = (imageString) => (
-  <img
-    src={'img/' + imageString + '.jpg'}
-    className="navigation__direction-image"
-    alt={imageString}
-  />
-);
-
-const DirectionText = (textString) => (
-  <p className="navigation__direction-text">{textString}</p>
-);
+  return (
+    <div className="navigation">
+      <ManeuverImage maneuver={maneuver || 'unknown'} />
+      <Distance meters={distance.value} />
+      <TextInstructions htmlInstructions={htmlInstructions} />
+      {`startCoords ${startCoords.lat} - ${startCoords.lng}`}
+      {`endCoords ${endCoords.lat} - ${endCoords.lng}`}
+    </div>
+  );
+};
 
 class NavigationView extends Component {
   constructor(props) {
@@ -43,7 +76,7 @@ class NavigationView extends Component {
       loading: true,
       steps: null,
       currentStep: -1, // steps: start -> mcdonalds -> home
-      currentWaypoint: -1 // waypoint: inside step "start -> mcdonalds": they're "turn left after 500m" etc
+      currentWaypoint: -1 // waypoint: inside step "start -> mcdonalds": they're "turn left after 500m" etc (it's current steps[currentStep].directions[currentWaypoint])
     };
   }
 
@@ -63,7 +96,14 @@ class NavigationView extends Component {
   }
 
   render() {
-    const content = this.state.loading ? <Loading /> : null;
+    const { loading, steps, currentStep, currentWaypoint } = this.state;
+    const content = loading ? (
+      <Loading />
+    ) : (
+      <Navigation
+        directionStep={steps[currentStep].directions[currentWaypoint]}
+      />
+    );
 
     return <div className="navigation-view">{content}</div>;
   }
